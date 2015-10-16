4e1a22ae8697f63f61824c9d0ebd0bf5196b8fe3
Daniel Minshew <ofenixculpa@gmail.com>
2015-10-16 00:56:48 -0500

    some ui changes and added back res.send

--- server/routes/deviceReports.js
+++ server/routes/deviceReports.js
@@ -19,12 +19,12 @@ Router.route('deviceReport', {
 
     // Send image.
 
-    // res.send(200, {
-    //   // time: new Date(),  // Stay in time sync             | TODO this is causing a big error dump currently
+    res.send(201/*, {
+      // time: new Date(),  // Stay in time sync             | TODO this is causing a big error dump currently
     //   relay1: true,      // turn relay1 on                   | - due to client not having 'send' method
     //   relay2: false,     // turn relay1 off                  | not sure of the fix?
     //   led1: true,        // turn leds on/off
     //   led2: false,
-    // });
+}*/);
   }
 });
